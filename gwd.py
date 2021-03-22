c53395fe9f233fe7f58fec0e17df7fc7ea88476d
Navodit1603 <navoditmaheshwari@gmail.com>
2021-03-22 09:54:10 -0700

    database table row code

--- gwd.py
+++ gwd.py
@@ -1,3 +1,15 @@
 import sqlite3
 
 conn = sqlite3.connect('gwdata.db')
+
+# creating cursor
+c = conn.cursor()
+
+# create a table
+c.execute("""CREATE TABLE gwdata (
+
+    user text,
+    email text
+    )
+    """)
+
