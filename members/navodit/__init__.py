e9d28b7394acc25d2493c9669ac48af6885ac369
Navodit1603 <navoditmaheshwari@gmail.com>
2021-03-29 17:18:42 -0700

    Navodit bp lab page updated

--- members/navodit/__init__.py
+++ members/navodit/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, Flask
 import model
 
 
-members_navodit_bp = Blueprint('navodit', __name__, static_folder="static", template_folder="templates")
+members_navodit_bp = Blueprint('navodit', __name__, static_folder="bp_static", template_folder="templates")
 
 
 @members_navodit_bp.route("/")
