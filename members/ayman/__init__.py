e403990f9f8b2047617b547bb0aeb6a3a3dd4345
aymankazi <akazi3211@gmail.com>
2021-04-02 21:53:11 -0700

    Completed Minilab

--- members/ayman/__init__.py
+++ members/ayman/__init__.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, Flask
+from flask import Blueprint, render_template, Flask, request
 import model
+from members.ayman.lab1 import Factor
+from builtins import int
 
 members_ayman_bp = Blueprint('ayman', __name__, static_folder="static", template_folder="templates")
 
@@ -7,3 +9,9 @@ members_ayman_bp = Blueprint('ayman', __name__, static_folder="static", template
 @members_ayman_bp.route("/")
 def ayman():
     return render_template("ayman.html", model=model.setup())
+
+@members_ayman_bp.route("/lab1/", methods=["GET", "POST"])
+def lab1():
+    if request.form:
+        return render_template("lab1.html", list = Factor(int(request.form.get("list"))))
+    return render_template("lab1.html", list = Factor(50000))
---
+++ members/ayman/lab1.py
@@ -0,0 +1,39 @@
+# factors of a number in a class
+class Factor:
+    def __init__(self, num):
+        if num < 0 or num > 100000:
+            raise ValueError("Value must be between 0 and 100 000")
+
+        self._list = []
+
+        #self.factor_number()
+        factor = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                print(i)
+                factor.append(i)
+        self._list.append(factor)
+
+
+    @property
+    def list(self):
+        return self._list
+
+if __name__ == "__main__":
+    while True:
+        try:
+            # asks user for an integer value
+            num = int(input('Enter a number to find its factors: '))
+            # if user doesn't input an integer or inputs a number larger than 50, it will raise the ValueError
+            if num > 100000:
+                raise ValueError
+            break
+        # ValueError sends an error message
+        except ValueError:
+            print('Invalid value or value too large. Value must be between 0 and 100 000. Please try again.')
+    # if value is successful, then it runs user_input through the pascal's triangle function
+    list = Factor(num)
+    print(list.list)
+
+
+
