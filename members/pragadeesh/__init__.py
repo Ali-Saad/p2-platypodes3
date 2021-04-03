350fbae55348f97fa3b98d1046f4591757566071
Pragadeesh Raj <pragadeesh3000@gmail.com>
2021-04-02 21:07:32 -0700

    Pascal's Triangle Complete

--- members/pragadeesh/__init__.py
+++ members/pragadeesh/__init__.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, Flask
-import model
+from builtins import int
 
+from flask import Blueprint, render_template, request
+import model
+from members.pragadeesh.pascal import Pascal
 
 members_pragadeesh_bp = Blueprint('pragadeesh', __name__, static_folder="static", template_folder="templates")
 
@@ -9,6 +11,9 @@ members_pragadeesh_bp = Blueprint('pragadeesh', __name__, static_folder="static"
 def pragadeesh():
     return render_template("prag.html", model=model.setup())
 
-@members_pragadeesh_bp.route("/pascal/")
-def pascal():
-    return render_template("pascal.html", model=model.setup())
+
+@members_pragadeesh_bp.route("/pascal/", methods=["GET", "POST"])
+def lab1():
+    if request.form:
+        return render_template("pascal.html", pascal=Pascal(int(request.form.get("row"))))
+    return render_template("pascal.html", pascal=Pascal(25))
--- members/pragadeesh/pascal.py
+++ members/pragadeesh/pascal.py
@@ -1,40 +1,53 @@
 # pascal triangle algorithm within a class
+from builtins import ValueError, range, print, int, property, input
+
+
 class Pascal:
     # function defined for pascal's triangle with parameter 'n'. This is the class object
-    def __init__(self):
+
+    def __init__(self, num):
+        if num < 0 or num > 25:
+            raise ValueError('Invalid Value')
+        self._pascal = []
+
         # the range has n + 1 instead of n because range function (1, n - 1) interval
         # therefore, 1 must be added for just n
         # ex: n = 5. The range goes from 1, 5 and produces 5 rows. Using only (1, n) would only give 4 rows
-        for row in range(1, self + 1):
-            self._row = row
+        for row in range(1, num + 1):
             # variable for starting value. Changing this will make the same pattern but with different starting values
             start = 1
+            row_l = []
             # ex: if n = 5. There are 5 rows. This loop would go through i as 1, 2, 3, 4, and 5
             for i in range(1, row + 1):
                 # first number in every line is 1 (start = 1)
                 print(start, end=" ")
+                row_l.append(start)
                 start = int(start * (row - i) / i)
+            self._pascal.append(row_l)
             print("")
 
     @property
-    def get_row(self):
-        return self._row
+    def pascal(self):
+        return self._pascal
 
 
+# testing with user inputs
 # lines 16-27 for checking valid integer values
-while True:
-    try:
-        # asks user for an integer value
-        user_input = int(input('Please enter an integer value: '))
-        # if user doesn't input an integer or inputs a number larger than 50, it will raise the ValueError
-        if user_input > 50:
-            raise ValueError
-        break
-    # ValueError sends an error message
-    except ValueError:
-        print('Invalid value or value too large. Please try again.')
-# if value is successful, then it runs user_input through the pascal's triangle function
-Pascal.__init__(user_input)
+if __name__ == "__main__":
+    while True:
+        try:
+            # asks user for an integer value
+            user_input = int(input('Please enter an integer value: '))
+            # if user doesn't input an integer or inputs a number larger than 50, it will raise the ValueError
+            if user_input > 25:
+                raise ValueError
+            break
+        # ValueError sends an error message
+        except ValueError:
+            print('Invalid value or value too large. Please try again.')
+    # if value is successful, then it runs user_input through the pascal's triangle function
+    pascal = Pascal(user_input)
+    print(pascal.pascal)
 
 '''pascal's triangle is used to find binomial coefficients.
 For example, (x+1)^6 will have the coefficients of the 
