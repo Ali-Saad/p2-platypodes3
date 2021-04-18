0d5469a8f0830efff3759640530b7aac3d4efe8e
aymankazi <akazi3211@gmail.com>
2021-04-18 11:11:00 -0700

    Login/signup frontend with basic backend

--- view.py
+++ view.py
@@ -4,7 +4,7 @@ import sqlite3 as sl3
 
 #import stats
 import storefb
-#import storesignup
+import storesignup
 # import storelogin
 from members.navodit import members_navodit_bp
 from members.pragadeesh import members_pragadeesh_bp
@@ -60,9 +60,38 @@ def trends_route():
 def login_route():
     return render_template("login.html", model=model.setup())
 
+@app.route('/login', methods=['POST'])
+def login():
+    mailadd = request.form['email']
+    psswrd = request.form['psw']
+
+    print('Login Page Username:', mailadd)
+    print('Login Page Password:', psswrd)
+    result = storesignup.logincheck(mailadd, psswrd)
+    if result == "yes":
+        return render_template("home.html", model=model.setup())
+    else:
+        return render_template("login.html", msg='Invalid Username or Password', model=model.setup())
+
 @app.route('/register/')
 def register_route():
     return render_template("register.html", model=model.setup())
+
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    mailadd = request.form['email']
+    psswrd = request.form['psw']
+    pssrep = request.form['psw-repeat']
+
+    storesignup.insertsignup(mailadd, psswrd, pssrep)
+    '''
+    print (fname)
+    print (lname)
+    print (mailid)
+    print(service)
+    print(opinion)
+    '''
+    return render_template("login.html", model=model.setup())
 
 @app.route('/feedback/')
 def fb_route():
