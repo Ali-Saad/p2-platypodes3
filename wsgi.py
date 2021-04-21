9675bcb682f7876d4de2963213fd823b1bf321c8
Pragadeesh Raj <pragadeesh3000@gmail.com>
2021-04-20 22:15:26 -0700

    Dark Mode + Other minor updates

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from view import app  # imports app to run the app routes from view
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port='8080')  # main run engine of website
+    app.run(debug=True, host='127.0.0.1', port='5000')  # main run engine of website
 # runs on LAN with port 8080
