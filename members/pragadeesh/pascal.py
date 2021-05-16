b2a448c0494839583dbbb0c47196009ed4083b5c
Pragadeesh Raj <pragadeesh3000@gmail.com>
2021-05-16 15:25:07 -0700

    Pascal comments for the create task

--- members/pragadeesh/pascal.py
+++ members/pragadeesh/pascal.py
@@ -1,30 +1,33 @@
-class pascalTriangle:
+class pascalTriangle:  # Class is defined
 
-    def __init__(self, num):
+    def __init__(self, num):  # Initialization procedure is defined; processes object num
         if num < 1 or num > 50:
-            raise ValueError('Invalid Value')
-        self._pascalList = []
+            raise ValueError('Invalid Value')  # Uses selection to check for a valid value
+        self._pascalList = []  # Program limits to 50 rows to avoid overflow on screen (frontend)
         matrix = [[0 for x in range(num)]
                   for y in range(num)]
 
-        for row in range(0, num):
-            rowlist = []
+        # Algorithm inspired by GeeksforGeeks
+        # Matrix multiplication logic from Programiz
+
+        for row in range(0, num):  # Iteration (for loop) runs through each index in the range and calculates the next row's values
+            rowlist = []  # list for capturing each row is defined (list modified by algorithm)
             for column in range(0, row + 1):
 
-                if column == 0 or column == row:
+                if column == 0 or column == row:  # Selection (if/else) is used to determine if the index is at the ends
                     matrix[row][column] = 1
-                    print(matrix[row][column], end=" ")
-                    rowlist.append(matrix[row][column])
+                    print(matrix[row][column], end=" ")  # Printed when running tester code
+                    rowlist.append(matrix[row][column])  # "rowlist" appends for column beginnings/ends
 
                 else:
                     matrix[row][column] = (matrix[row - 1][column - 1] + matrix[row - 1][column])
-                    print(matrix[row][column], end=" ")
-                    rowlist.append(matrix[row][column])
+                    print(matrix[row][column], end=" ")  # Printed when running tester code
+                    rowlist.append(matrix[row][column])  # "rowlist" appends for middle columns
             print("\n", end="")
-            self._pascalList.append(rowlist)
+            self._pascalList.append(rowlist)  # Appends formatted data from "rowlist" into the list
 
-    @property
-    def pascalList(self):
+    @property  # Property decorator
+    def pascalList(self):  # Program's list is defined here for use in the backend
         return self._pascalList
 
 
@@ -32,20 +35,14 @@ class pascalTriangle:
 if __name__ == "__main__":
     while True:
         try:
-            # asks user for an integer value
-            user_input = int(input('Please enter an integer value: '))
-            # if user doesn't input an integer or inputs a number larger than 50, it will raise the ValueError
+            user_input = int(input('Please enter an integer value: '))  # Asks user for integer value
+            # If user doesn't input an integer or inputs a number larger than 50, it will raise the ValueError
             if user_input > 50:
                 raise ValueError
             break
         # ValueError sends an error message
         except ValueError:
             print('Invalid value or value too large. Please try again.')
-    # if value is successful, then it runs user_input through the pascal's triangle function
-    pascal = pascalTriangle(user_input)     # Pascal = class
-    print(pascal.pascalList)
-
-
-    '''num = int(input("Enter the number of rows: "))
-    pascal = pascalTriangle(num)
-    print(pascal.pascalList)'''
+    # If value is successful, then it runs user_input through the pascal's triangle function
+    pascal = pascalTriangle(user_input)  # Call to the pascalTriangle procedure
+    print(pascal.pascalList)  # Prints the final appended list "pascalList"
