9b805b0f332d7233e0cab08c9c3f7200712ba5a1
mustafasharaf <mustafasharaf03@gmail.com>
2021-05-20 19:42:12 -0700

    added bubblesort and updated mustafa.html

--- members/mustafa/__init__.py
+++ members/mustafa/__init__.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, Flask, request
 import model
-
+from members.mustafa.bubblesort import Bubblesort
 
 members_mustafa_bp = Blueprint('mustafa', __name__, static_folder="bp_static", template_folder="templates")
 
 
-@members_mustafa_bp.route("/")
+@members_mustafa_bp.route("/", methods=["GET", "POST"])
 def mustafa():
-    return render_template("mustafa.html", model=model.setup())
+    if request.form:
+        return render_template("mustafa.html", bubble=Bubblesort(request.form.get("sort")))
+    return render_template("mustafa.html", bubble=Bubblesort("42, 7, 9, 2"))
+
