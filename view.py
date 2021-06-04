dfa31486cfeb369444479aedcf11ea05d266a6b1
Pragadeesh Raj <pragadeesh3000@gmail.com>
2021-06-04 11:18:35 -0700

    Got the API to work. Fixed imports and organized view in general

--- view.py
+++ view.py
@@ -1,28 +1,35 @@
 from flask import Flask, render_template, request
-#from chatterbot import ChatBot
-#from chatterbot.trainers import ChatterBotCorpusTrainer
-#from chatterbot.trainers import ListTrainer
+from flask_sqlalchemy import SQLAlchemy
+# import json
 import model
-import sqlite3 as sl3
-
-# import stats
+from subs import new
 import storefb
 import storesignup
 # import storelogin
 from mapalgo import Color
-
 # Import Blueprints
 from members.navodit import members_navodit_bp
 from members.pragadeesh import members_pragadeesh_bp
 from members.ayman import members_ayman_bp
 from members.ali import members_ali_bp
 from members.mustafa import members_mustafa_bp
+from flask_migrate import Migrate
+from flask_restful import Resource, Api
 
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot.trainers import ListTrainer
 # import sqlite3 as sl3
-from subs import new
 
-# Blueprints
 app = Flask(__name__)
+db = SQLAlchemy()
+dbURI = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+db = SQLAlchemy(app)
+Migrate(app, db)
+api = Api(app)
 app.register_blueprint(members_navodit_bp, url_prefix='/navodit')
 app.register_blueprint(members_pragadeesh_bp, url_prefix='/pragadeesh')
 app.register_blueprint(members_ali_bp, url_prefix='/ali')
@@ -30,6 +37,44 @@ app.register_blueprint(members_ayman_bp, url_prefix='/ayman')
 app.register_blueprint(members_mustafa_bp, url_prefix='/mustafa')
 
 
+class Info(db.Model):  # Basic Info Database
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    carbon_footprint = db.Column(db.Integer)
+    paris_agreement = db.Column(db.String(50))
+
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "carbon_footprint": self.carbon_footprint,
+            "paris_agreement": self.paris_agreement
+        }
+
+    # class for create/post
+    class Create(Resource):
+        def post(self, name, carbon_footprint, paris_agreement):
+            country = model_create(name, carbon_footprint, paris_agreement)
+            if country:
+                return country.json()
+            return {'failed': None}, 404  # need to have better message on errors
+
+    api.add_resource(Create, '/api/add_info/<string:name>/<int:carbon_footprint>/<string:paris_agreement>')
+
+
+def model_create(name, carbon_footprint, paris_agreement):
+    """prepare data for primary table extracting from form"""
+    try:
+        country = Info(
+            name=name,
+            carbon_footprint=carbon_footprint,
+            paris_agreement=paris_agreement
+        )
+        db.session.add(country)
+        db.session.commit()
+        return country
+    except:
+        return None
 
 
 @app.route('/blueprint/')
@@ -63,6 +108,8 @@ def info_route():
 @app.route('/info/faq/')
 def faq_route():
     return render_template("faq.html", model=model.Faq())
+
+
 # experimenting with backend above
 
 
@@ -172,6 +219,7 @@ def map():
         return render_template("map.html", color=Color(request.form.get("int")))
     return render_template("map.html", color=Color(200))
 
+
 """"bot = ChatBot("Candice")
 bot.set_trainer(ListTrainer)
 bot.train(['What is your name?', 'My name is Candice'])
@@ -189,6 +237,7 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(bot.get_response(userText))"""
 
+
 @app.route('/memes/')
 def meme_route():
     return render_template("meme.html")
@@ -199,6 +248,32 @@ def subscribe():
     return new(request)
     return render_template("home.html")
 
+
 @app.route('/hotspots/')
 def hot_route():
     return render_template("hotspot.html")
+
+
+'''@app.route('/api/add_info', methods=['POST'])
+def add_info():
+    info_data = request.get_json()
+    print(info_data)
+
+    new_info = Info(name=info_data['name'], carbon_footprint=info_data['carbon_footprint'],
+                    paris_agreement=info_data['paris_agreement'])
+
+    db.session.add(new_info)
+    db.session.commit()
+
+    return 'Done', 201'''
+
+
+@app.route('/api/info')
+def info():
+    countries = Info.query.all()
+    list = [country.json() for country in countries]
+    return {"list": list}
+
+
+if __name__ == "__main__":
+    db.create_all()
